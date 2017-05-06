c81018006943ca36cee72de84c7be3b4e4eb4207
Hartman <imh29@drexel.edu>
2017-05-06 11:20:59 -0400

    moving to json based config, adding schema validation

--- fruitbasket/configparser.py
+++ fruitbasket/configparser.py
@@ -1,5 +1,8 @@
 import ConfigParser
+import json
+import jsonschema
 import os
+import pprint
 
 # Configuration files will always be relative to the parser file
 # Instead of attemping to find the path settings/application.conf relative to the file that made the call
@@ -12,6 +15,34 @@ CONFIG_FILES = ['configuration.conf']
 for i, f in enumerate(CONFIG_FILES):
     CONFIG_FILES[i] = os.path.join(settingspath, f)
 
+
+def fullJsonPath(filename):
+    return os.path.join(settingspath, 'json', filename)
+
+# ----------------------------------------------------------------------------------------------------
+# JSON FILE PARSER
+# ----------------------------------------------------------------------------------------------------
+
+
+def readJson(filename='configuration.json'):
+    if os.path.splitext(filename)[1] != '.json':
+        filename = filename + '.json'
+
+    file_path = fullJsonPath(filename)
+
+    # If it doesn't exist in the correct settings/json folder
+    # return without attempting to open a file that doesn't exist
+    if not os.path.isfile(file_path):
+        print 'JSON File [%s] does not exist.'
+        return 1
+
+    with open(file_path) as data_file:
+        data = json.load(data_file)
+        return data
+
+# ----------------------------------------------------------------------------------------------------
+# CONF FILE PARSER
+# ----------------------------------------------------------------------------------------------------
 
 config = ConfigParser.SafeConfigParser()
 
@@ -64,3 +95,7 @@ def setvalue(section, option, value, name='configuration'):
 def get_sections(name='configuration'):
     readfile(name)
     return config.sections()
+
+if __name__ == "__main__":
+    config_data = readJson()
+    pprint.pprint(config_data)
